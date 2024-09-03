27f91fc461f0a1267fd6ade610d48877fdcee935
lucia nambi <nambilucia@gmail.com>
2024-09-03 16:35:39 +0300

    add morgan middeleware to log requests and logs folder

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express =require("express");
-
-
+const fs = require('fs')
+const morgan =require('morgan')
+const path =require('path');
 
 
 const app =express();
 const PORT =5000;
 
+//middeleware
+app.use(express.json());
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'request_logs.txt'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 
 
 
+app.get("/",(req,res)=>{
+return res.send("<h1>Welcome to the Afaazi Server page</h1>")
+})
 
 app.listen(PORT,()=>{
   console.log(`Server is listening on http://localhost:${PORT}`)
