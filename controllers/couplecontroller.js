53344274d9748e6e84935e76f0c19674f3182d77
lucia nambi <nambilucia@gmail.com>
2024-09-09 12:40:35 +0300

    add couple sign up and login

--- controllers/couplecontroller.js
+++ controllers/couplecontroller.js
@@ -1,6 +1,9 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const { StatusCodes } = require("http-status-codes");
+const jwt =require('jsonwebtoken');
+require('dotenv/config');
+const bcrypt= require('bcrypt');
 
 
 const getCouples = async (req, res) => {
@@ -13,7 +16,87 @@ const getCouples = async (req, res) => {
         .json({ error: error.message });
     }
   };
+  //signup
+
+  const signUp =async (req,res)=>{
+      try{
+        const {username,fullname,email,telephone,password}=req.body;
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const newCouple = await prisma.couple.create({
+          data:{
+            username,
+            fullname,
+            email,
+            telephone,
+            password:hashedPassword
+
+          }
+
+        })
+
+        return res.status(StatusCodes.OK).json({"message":"New couple created successfully", newCouple})
+
+      }
+      catch(error){
+        console.log(error)
+return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({"message":"Error occured while creating new couple"})
+
+      }
+
+  }
+
+
+  //login
+
+const login = async (req,res)=>{
+  try{
+    const {username,password}=req.body;
+    let couple= await prisma.couple.findUnique({
+      where:{
+        username
+      }
+    });
+    if(couple){
+      const matchPassword =await bcrypt.compare(password,couple.password)
+      if(matchPassword){
+        //create token
+        const coupleToken = await jwt.sign(
+          {id:couple.id,username:couple.username},
+          process.env.SECRET_KEY,
+          {expiresIn:"1h"}
+
+        )
+        //send token back as response
+        res.status(StatusCodes.OK).json({coupleToken,"message":"Successful Couple login"})
+
+      }
+      else{
+        return res.status(StatusCodes.UNAUTHORIZED).json({"error":"Wrong password"});
+      }
+
+    
+    }
+
+    else{
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({"error":"Couple not found",couple});
+    }
+
+
+  }
+  catch{
+    return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ error: error.message });
+
+
+  }
+
+}
+
 
   module.exports = {
-    getCouples
+    getCouples,
+    signUp,
+    login
 }
--- routes/couplesRoute.js
+++ routes/couplesRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 //deal with requests
 const couplesRoute = express.Router();
-const {getCouples}= require("../controllers/couplecontroller");
+const {getCouples,signUp,login}= require("../controllers/couplecontroller");
 
 
 //couple requests
 couplesRoute.get("/",getCouples);
+couplesRoute.post("/signup",signUp);
+couplesRoute.post("/login",login);
 
 
 module.exports = {
