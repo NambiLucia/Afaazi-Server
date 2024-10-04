d141f2d0015e23c11cb4dbe0199315af4dfcfad2
lucia nambi <nambilucia@gmail.com>
2024-10-04 10:02:57 +0300

    add supertest and jest

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
-const express =require("express");
-const fs = require('fs')
-const morgan =require('morgan')
-const path =require('path');
-var cors = require('cors')
-const {couplesRoute} = require("./routes/couplesRoute");
-const {vendorsRoute} = require("./routes/vendorsRoute");
-const {categoryRoute} = require("./routes/categoryRoute");
-const { bookingsRoute } = require("./routes/bookingsRoute");
+const app = require('./server')
 
-
-const app =express();
 const PORT = 5000;
-//process.env.PORT 
+//process.env.PORT
 
-
-//middeleware
-app.use(express.json());
-app.use(cors());
-
-// create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'request_logs.txt'), { flags: 'a' })
- 
-// setup the logger
-app.use(morgan('combined', { stream: accessLogStream }))
-
-
-//middelware to direct endpoint requests to couplesRoute
-app.use("/couples",couplesRoute);
-app.use("/vendors",vendorsRoute);
-app.use("/categories",categoryRoute);
-app.use("/bookings",bookingsRoute); 
-
-
-
-
-
-app.get("/",(req,res)=>{
-return res.send("<h1>Welcome to the Afaazi Server page</h1>")
-})
-
-app.listen(PORT,()=>{
-  console.log(`Server is listening on http://localhost:${PORT}`)
-
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs");
+const morgan = require("morgan");
+const path = require("path");
+var cors = require("cors");
+const { couplesRoute } = require("./routes/couplesRoute");
+const { vendorsRoute } = require("./routes/vendorsRoute");
+const { categoryRoute } = require("./routes/categoryRoute");
+const { bookingsRoute } = require("./routes/bookingsRoute");
+
+const app = express();
+
+//middeleware
+app.use(express.json());
+app.use(cors());
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "request_logs.txt"),
+  { flags: "a" }
+);
+
+// setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
+
+//middelware to direct endpoint requests to couplesRoute
+app.use("/couples", couplesRoute);
+app.use("/vendors", vendorsRoute);
+app.use("/categories", categoryRoute);
+app.use("/bookings", bookingsRoute);
+
+app.get("/", (req, res) => {
+  return res.send("<h1>Welcome to the Afaazi Server page</h1>");
+});
+
+module.exports = app;
