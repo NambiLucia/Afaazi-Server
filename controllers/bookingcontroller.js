dcc818e660de3bc44986be99d1f9fe08b01d6bff
lucia nambi <nambilucia@gmail.com>
2024-10-15 13:11:08 +0300

    add get booking by vendor Id

--- controllers/bookingcontroller.js
+++ controllers/bookingcontroller.js
@@ -1,183 +1,248 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const { StatusCodes } = require("http-status-codes");
-const toISODateString =require("../Utils/dateUtils")
-const generateSlug = require("../Utils/slugUtils")
-
-
+const toISODateString = require("../Utils/dateUtils");
+const generateSlug = require("../Utils/slugUtils");
+const jwt = require("jsonwebtoken");
 
 const getBookings = async (req, res) => {
-    try {
-      let bookings = await prisma.booking.findMany({
-        include:{
-          vendor:{
-            select:{
-              username:true,
-            }
-          }
-        
-        }
-  
-      });
-      res.json(bookings);
-    } catch (error) {
-      return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: error.message });
-    }
-  }; 
-
-  const getBookingsByCoupleId = async (req, res) => {
-    console.log('Request received for coupleId:', req.params.coupleId); 
-    try{
-      const coupleId = parseInt(req.params.coupleId);
-  
-      const bookings = await prisma.booking.findMany({
-        where: {
-          coupleId: coupleId,
+  try {
+    let bookings = await prisma.booking.findMany({
+      include: {
+        vendor: {
+          select: {
+            username: true,
+          },
         },
-      });
-
-      const couple = await prisma.couple.findUnique({
-        where: {
-            id: coupleId,
-        },
-        select: {
-            fullname: true, 
-        },
+      },
     });
-   
-      return res.status(StatusCodes.OK).json({ message: `Bookings for couple ID ${coupleId} - ${couple.fullname}`,bookings})
-   
-  
-    }
-  catch(error){
+    res.json(bookings);
+  } catch (error) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({message: 'An error occurred while fetching bookings',
-        error: error.message || error});
-  
+      .json({ error: error.message });
   }
-  };
+};
+
+const getBookingsByCoupleId = async (req, res) => {
+  console.log("Request received for coupleId:", req.params.id);
+  try {
+    const id = parseInt(req.params.id);
+
+    const bookings = await prisma.booking.findMany({
+      where: {
+        id: id,
+      },
+      include: {
+        couple: {
+          select: {
+            fullname: true,
+          },
+        },
+        vendor: {
+          select: {
+            fullname: true,
+          },
+        },
+      },
+    });
+
+    return res
+      .status(StatusCodes.OK)
+      .json({
+        message: `Bookings for couple ID ${id} - ${bookings[0]?.couple.fullname}`,
+        bookings,
+      });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        message: "An error occurred while fetching bookings",
+        error: error.message || error,
+      });
+  }
+};
+
+const getBookingsByVendorId = async (req, res) => {
+  console.log('Request received for vendorId:', req.params.id); 
+  try{
+    const id = parseInt(req.params.id);
+
+    const bookings = await prisma.booking.findMany({
+      where: {
+        id: id,
+      },
+      include:{
+        couple:{
+          select:{
+            fullname:true,
+          },
+        },
+        vendor:{
+          select:{
+            fullname:true
+          }
+        }
+
+      },
 
 
 
 
 
 
-  
-  const getBookingsBySlug = async (req, res) => {
-    try{
-     const slug = req.params.slug
+    });
+
+ 
+    return res.status(StatusCodes.OK).json({ message: `Bookings for Vendor ID ${id} - ${bookings[0]?.vendor.fullname}`, bookings });
+
+
+  }
+catch(error){
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({message: 'An error occurred while fetching Vendor bookings',
+      error: error.message || error});
+
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getBookingsBySlug = async (req, res) => {
+  try {
+    const slug = req.params.slug;
     const getBooking = await prisma.booking.findUnique({
-      where:{
-        slug:slug
-      }
-    })
-   
-   
-      return res.status(StatusCodes.OK).json({ message: `Bookings By slug ${slug}`,getBooking})
-   
-    }
-  catch(error){
+      where: {
+        slug: slug,
+      },
+    });
+
     return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({error});
-  
+      .status(StatusCodes.OK)
+      .json({ message: `Bookings By slug ${slug}`, getBooking });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
   }
-  };
-  
+};
 
+//Create booking
 
-  //Create booking
+const createBooking = async (req, res) => {
+  try {
+    const coupleId = req.coupleId;
+    const {
+      username,
+      fullname,
+      email,
+      telephone,
+      eventDate,
+      eventType,
+      country,
+      city,
+      estimatedBudget,
+      additionalInfo,
+      vendorId,
+    } = req.body;
 
-  const createBooking =async (req,res)=>{
-      try{
-        const coupleId = req.coupleId
-        const { username,fullname,email,telephone,eventDate,eventType,country,city,estimatedBudget,additionalInfo,vendorId}=req.body;
-        
-        const eventDateISO = toISODateString(eventDate)
-         if (!eventDateISO) {
+    const eventDateISO = toISODateString(eventDate);
+    if (!eventDateISO) {
       return res.status(400).json({ message: "Invalid event date format." });
     }
-        const slug = generateSlug(username);
-      
-        const newBooking = await prisma.booking.create({
-          data:{
-          username,slug,fullname,email,telephone,eventDate:eventDateISO,eventType,country,city,estimatedBudget,additionalInfo, coupleId,vendorId
+    const slug = generateSlug(username);
 
-          }
+    const newBooking = await prisma.booking.create({
+      data: {
+        username,
+        slug,
+        fullname,
+        email,
+        telephone,
+        eventDate: eventDateISO,
+        eventType,
+        country,
+        city,
+        estimatedBudget,
+        additionalInfo,
+        coupleId,
+        vendorId,
+      },
+    });
 
-
-        })
-
-
-        return res.status(StatusCodes.OK).json({"message":"Event booked successfully", newBooking})
-
-      }
-      catch(error){
-        console.log(error)
-return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({"message":"Error occured while booking event"})
-
-      }
-
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "Event booked successfully", newBooking });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error occured while booking event" });
   }
+};
 
-  const updateBookingsById = async (req, res) => {
-    try {
-      const updatedBooking = await prisma.booking.update({
-        where: {
-          id: parseInt(req.params.id),
-        },
-        data: req.body,
-      });
-      if (!updatedBooking) {
-        return res.status(404).json({ error: "Booking not found" });
-      }
-      return res.status(StatusCodes.OK).json({message:`Booking updated`,updatedBooking});
-    } catch (error) {
-      return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: error.message });
+const updateBookingsById = async (req, res) => {
+  try {
+    const updatedBooking = await prisma.booking.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: req.body,
+    });
+    if (!updatedBooking) {
+      return res.status(404).json({ error: "Booking not found" });
     }
-  };
-  
-  const deleteBookingsById = async (req, res) => {
-    try {
-      const deletedBooking = await prisma.booking.delete({
-        where: {
-          id: +(req.params.id),
-        },
-      });
-  
-  if(deletedBooking){
-     return res
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: `Booking updated`, updatedBooking });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
+const deleteBookingsById = async (req, res) => {
+  try {
+    const deletedBooking = await prisma.booking.delete({
+      where: {
+        id: +req.params.id,
+      },
+    });
+
+    if (deletedBooking) {
+      return res
         .status(StatusCodes.OK)
         .json({ message: "Booking deleted", deletedBooking });
-  
-  }
-  else{
-    return res.status(404).json({ error: "Sorry,Booking doesnt exist" });
-  }
-  
-  
-     
-    } catch (error) {
-      return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: error.message });
+    } else {
+      return res.status(404).json({ error: "Sorry,Booking doesnt exist" });
     }
-  };
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
 
-
-
-
-
-  module.exports = {
-    getBookings,
-    getBookingsByCoupleId,
-    getBookingsBySlug,
-    createBooking,
-    updateBookingsById,
-    deleteBookingsById
-}
+module.exports = {
+  getBookings,
+  getBookingsByCoupleId,
+  getBookingsByVendorId,
+  getBookingsBySlug,
+  createBooking,
+  updateBookingsById,
+  deleteBookingsById,
+};
--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 //deal with requests
 const bookingsRoute = express.Router();
-const {getBookings,getBookingsByCoupleId,getBookingsBySlug,createBooking,updateBookingsById,deleteBookingsById}= require("../controllers/bookingcontroller");
+const {getBookings,getBookingsByCoupleId,getBookingsByVendorId,getBookingsBySlug,createBooking,updateBookingsById,deleteBookingsById}= require("../controllers/bookingcontroller");
 const {validateSchema} = require('../Utils/joi-validator');
 const { bookingSchema } = require("../Utils/joi-schemas");
 const validateToken = require("../Utils/validateToken");
@@ -9,7 +9,8 @@ const validateToken = require("../Utils/validateToken");
 
 //couple requests
 bookingsRoute.get("/",getBookings);
-bookingsRoute.get("/couples/:coupleId",getBookingsByCoupleId);
+bookingsRoute.get("/couple/:id",getBookingsByCoupleId);
+bookingsRoute.get("/vendor/:id",getBookingsByVendorId);
 bookingsRoute.get("/:slug",getBookingsBySlug);
 bookingsRoute.post("/create-booking",validateSchema(bookingSchema),validateToken,createBooking);
 bookingsRoute.put("/:id",validateSchema(bookingSchema),updateBookingsById);
